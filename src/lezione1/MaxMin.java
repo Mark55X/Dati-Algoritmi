e6ba5ff9dc4bc497eff362f416f02eea8802af5b
Marco <marco2002stefani@gmail.com>
2022-10-04 17:56:29 +0200

    ok maxMin

--- src/lezione1/MaxMin.java
+++ src/lezione1/MaxMin.java
@@ -3,31 +3,34 @@ package lezione1;
 public class MaxMin {
 
 	public static void main(String[] args) {
-		int[] array = {4,8,1,4,20,5,9};
+		
+		int[] array = {4,-6,1,4,5,5000};
 		if(array.length == 0) {
 			System.out.print("Nessun elemento");
 			return;
 		}
-		
-				
-		int maxsLength = array.length % 2 == 0 ? array.length / 2 : array.length + 1;
-		int [] maxs = new int[maxsLength];
+								
+		int [] maxs = new int[array.length / 2 + 1];
 		int [] mins = new int[array.length / 2];
 		int indexMax = 0, indexMin = 0;
-		
-		
-		//ALGORIMTO DI (3n/2) - 2 confronti 
+				
+		//ALGORIMTO DI (3n/2) - 1 confronti 
 		
 		// n/2 confronti
-		for(int i = 0;i < array.length; i+=2) {
+		for(int i = 0;i < array.length - 1; i+=2) {
 			if(array[i] > array[i+1]) {
 				maxs[indexMax++] = array[i];
+				mins[indexMin++] = array[i+1];
 			}else {
-				mins[indexMin++] = array[i + 1];
+				mins[indexMin++] = array[i];
+				maxs[indexMax++] = array[i + 1];
 			}
 		}
 		
-		// maxsLength -1 confronti
+		//Aggiunta Utile se numero di elementi dell'array dispari, se no inutile
+		maxs[maxs.length - 1] = array[array.length -1 ]; 
+		
+		// n/2 confronti
 		int max = maxs[0] , min = mins[0];
 		for(int i = 1; i < maxs.length; i++) {
 			if(maxs[i] > max) {
@@ -37,8 +40,8 @@ public class MaxMin {
 		
 		// n/2 - 1 confronti
 		for(int i = 1; i < mins.length; i++) {
-			if(mins[i] > min) {
-				max = mins[i];
+			if(mins[i] < min) {
+				min = mins[i];
 			}
 		}
 		
